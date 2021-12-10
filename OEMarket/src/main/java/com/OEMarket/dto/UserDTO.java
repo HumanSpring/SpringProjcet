b0d80e42d7d357c308c1f41dfc09fb1527abb029
SLCube <kgm0096@naver.com>
2021-12-10 21:24:08 +0900

    프로젝트 생성 + 오라클DB연결 및 테스트 성공 + DTO클래스 생성 + log4jdbc 세팅 + 트랜잭션 세팅 + LoggerAspect 세팅

---
+++ OEMarket/src/main/java/com/OEMarket/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.OEMarket.dto;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+	/* 회원 번호(PK) */
+	private Long userNo;
+	
+	/* 아이디 */
+	private String id;
+	
+	/* 비밀번호 */
+	private String password;
+	
+	/* 이름 */
+	private String name;
+	
+	/* 닉네임 */
+	private String nickName;
+	
+	/* 성별 */
+	private String gender;
+	
+	/* 전화번호 */
+	private String phone;
+	
+	/* 회원아이콘 */
+	private String userIconName;
+	
+	/* 회원가입날짜 */
+	private Date signupDate;
+	
+	/* 회원탈퇴여부 */
+	private String signoutYn;
+	
+	/* 회원탈퇴날짜 */
+	private Date signoutDate;
+	
+	/* 관리자 */
+	private String manager;
+	
+}
