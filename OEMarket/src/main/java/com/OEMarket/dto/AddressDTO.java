65b506c59362810205387137b4117e5e90722d49
antonio8041 <antonio107@naver.com>
2021-12-13 10:57:11 +0900

    add branch yun

--- OEMarket/src/main/java/com/OEMarket/dto/AddressDTO.java
+++ OEMarket/src/main/java/com/OEMarket/dto/AddressDTO.java
@@ -7,15 +7,16 @@ import lombok.Setter;
 @Setter
 public class AddressDTO extends CommonDTO {
 
-	/* 배송지 번호(PK) */
-	private Long addressNo;
-	
-	/* 회원번호 */
-	private Long userNo;
-	
-	/* 우편번호 */
-	private String postalCode;
-	
-	/* 상세주소 */	
-	private String detailAddress;
+    /* 배송지 번호(PK) */
+    private Long addressNo;
+
+    /* 회원번호 */
+    private Long userNo;
+
+    /* 우편번호 */
+    private String postalCode;
+
+    /* 상세주소 */
+    private String detailAddress;
+
 }
