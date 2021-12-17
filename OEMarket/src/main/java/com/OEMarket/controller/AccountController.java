a4c8a93150745f83f456ae292bb1a07675ac4680
sara3344 <secrethappy@naver.com>
2021-12-17 23:44:05 +0900

    account 프론트 작업

--- OEMarket/src/main/java/com/OEMarket/controller/AccountController.java
+++ OEMarket/src/main/java/com/OEMarket/controller/AccountController.java
@@ -5,14 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
+	
+	//회원가입
 	@GetMapping(value ="/account/join.do" )
 	public String AccountJoin() {
 		return "account/join";
 	}
 	
+	//로그인
 	@GetMapping(value ="/account/login.do" )
 	public String AccountLogin() {
 		return "account/login";
 	}
+	
+	//비밀번호 찾기
+	@GetMapping(value ="/account/find_pw.do" )
+	public String AccountFindPw() {
+		return "account/find_pw";
+	}
+	
+	//아이디 찾기
+	@GetMapping(value ="/account/find_id.do" )
+	public String AccountFindId() {
+		return "account/find_id";
+	}
 
+	//비밀번호 찾기 결과
+	@GetMapping(value ="/account/find_pw_result.do" )
+	public String AccountFindPwResult() {
+		return "account/find_pw_result";
+	}
+	
 }
