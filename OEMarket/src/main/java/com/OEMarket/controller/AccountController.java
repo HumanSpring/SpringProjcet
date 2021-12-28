27cd3a257b18649b106cd6a83f627ef3c02be8dc
SLCube <kgm0096@naver.com>
2021-12-28 11:37:45 +0900

    패키지 이름 대소문자가 왜자꾸 바뀌지

--- OEMarket/src/main/java/com/OEMarket/controller/AccountController.java
+++ OEMarket/src/main/java/com/OEMarket/controller/AccountController.java
@@ -1,4 +1,4 @@
-package com.OEMarket.Controller;
+package com.OEMarket.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
--- OEMarket/src/main/java/com/OEMarket/controller/BoardController.java
+++ OEMarket/src/main/java/com/OEMarket/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.OEMarket.Controller;
+package com.OEMarket.controller;
 
 import java.util.List;
 
